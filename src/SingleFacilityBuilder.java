783024b7708a17e0e17c44dfaaacedb02c9b44a3
= <william.ray@hsc.utah.edu>
2024-09-12 10:37:53 -0600

    add a builder and the code from the AnyLogic model

---
+++ src/SingleFacilityBuilder.java
@@ -0,0 +1,15 @@
+
+import repast.simphony.dataLoader.engine.ContextBuilderFactory;
+import repast.simphony.context.Context;
+import repast.simphony.dataLoader.ContextBuilder;
+
+
+public class SingleFacilityBuilder implements ContextBuilder<Object> {
+
+	@Override
+	public Context build(Context<Object> context) {
+		// TODO Auto-generated method stub
+		return context;
+	}
+
+}
