21a3ffb121dfb4fc87f1a1f6985c46f63d0d6b7e
= <william.ray@hsc.utah.edu>
2024-09-13 08:41:20 -0600

    add process abstracts and SimpleProcess implementation

--- src/processes/Process.groovy
+++ src/processes/Process.groovy
@@ -1,6 +1,40 @@
 package processes
 
-import processes.Process
+import org.apache.commons.math3.distribution.ExponentialDistribution;
+import agents.Agent
+import repast.simphony.engine.schedule.ISchedulableAction;
+import repast.simphony.engine.schedule.ISchedule;
+import java.lang.Math;
 
-abstract class Process {
+
+abstract class Process extends Agent{
+
+
+	ISchedule schedule
+	double meanIntraEventTime
+	double nextEventTime
+	ExponentialDistribution distro
+	ISchedulableAction nextAction
+	
+	
+
+	Process(double intra_event_time){
+		if (intra_event_time > 0) {
+			schedule = repast.simphony.engine.environment.RunEnvironment.getInstance().getCurrentSchedule();
+			meanIntraEventTime = intra_event_time
+			distro = new ExponentialDistribution(intra_event_time)
+		}
+	}
+
+	abstract def start()
+	
+	abstract def fire()
+
+	abstract def stop() 
+
+	double getNextEventTime(){
+		double currTime = schedule.getTickCount()
+		double elapse = distro.sample()
+		return Math.max(currTime+elapse,0)
+	}
 }
