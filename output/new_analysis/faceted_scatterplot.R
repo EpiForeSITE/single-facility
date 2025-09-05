5084a25e32d6a334e91713c311ddfb0aacb585f4
Willy Ray <william.ray@hsc.utah.edu>
2025-09-05 14:59:09 -0600

    add small sample dataset to new_analysis

--- output/new_analysis/faceted_scatterplot.R
+++ output/new_analysis/faceted_scatterplot.R
@@ -1,45 +1,32 @@
 # R script to create scatter plots for sensitivity analysis
 library(ggplot2)
 
+# Get command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+column <- "probSurveillanceDetection" # default
+if (length(args) > 0) {
+  for (arg in args) {
+    if (grepl("^column=", arg)) {
+      column <- sub("^column=", "", arg)
+    }
+  }
+}
+
 # Read merged data
 merged <- read.csv("merged.txt", header=TRUE)
 
-# NumberOfTransmissions vs probSurveillanceDetection
-p1 <- ggplot(merged, aes(x=probSurveillanceDetection, y=NumberOfTransmissions)) +
-  geom_point(size=3) +
-  labs(title="NumberOfTransmissions vs probSurveillanceDetection",
-       x="probSurveillanceDetection",
-       y="NumberOfTransmissions")
-ggsave("scatter_NumberOfTransmissions_vs_probSurveillanceDetection.png", p1)
+# Helper function to plot
+plot_scatter <- function(yvar, ylab, filename) {
+  p <- ggplot(merged, aes_string(x=column, y=yvar)) +
+    geom_point(size=3) +
+    labs(title=paste(ylab, "vs", column),
+         x=column,
+         y=ylab)
+  ggsave(filename, p)
+}
 
-# MeanDailyPrevalence vs probSurveillanceDetection
-p2 <- ggplot(merged, aes(x=probSurveillanceDetection, y=MeanDailyPrevalence)) +
-  geom_point(size=3) +
-  labs(title="MeanDailyPrevalence vs probSurveillanceDetection",
-       x="probSurveillanceDetection",
-       y="MeanDailyPrevalence")
-ggsave("scatter_MeanDailyPrevalence_vs_probSurveillanceDetection.png", p2)
-
-# ClinicalDetections vs probSurveillanceDetection
-p3 <- ggplot(merged, aes(x=probSurveillanceDetection, y=ClinicalDetections)) +
-  geom_point(size=3) +
-  labs(title="ClinicalDetections vs probSurveillanceDetection",
-       x="probSurveillanceDetection",
-       y="ClinicalDetections")
-ggsave("scatter_ClinicalDetections_vs_probSurveillanceDetection.png", p3)
-
-# MeanDischargePrevalence vs probSurveillanceDetection
-p4 <- ggplot(merged, aes(x=probSurveillanceDetection, y=MeanDischargePrevalence)) +
-  geom_point(size=3) +
-  labs(title="MeanDischargePrevalence vs probSurveillanceDetection",
-       x="probSurveillanceDetection",
-       y="MeanDischargePrevalence")
-ggsave("scatter_MeanDischargePrevalence_vs_probSurveillanceDetection.png", p4)
-
-# ImportationPrevalence vs probSurveillanceDetection
-p5 <- ggplot(merged, aes(x=probSurveillanceDetection, y=ImportationPrevalence)) +
-  geom_point(size=3) +
-  labs(title="ImportationPrevalence vs probSurveillanceDetection",
-       x="probSurveillanceDetection",
-       y="ImportationPrevalence")
-ggsave("scatter_ImportationPrevalence_vs_probSurveillanceDetection.png", p5)
+plot_scatter("NumberOfTransmissions", "NumberOfTransmissions", paste0("scatter_NumberOfTransmissions_vs_", column, ".png"))
+plot_scatter("MeanDailyPrevalence", "MeanDailyPrevalence", paste0("scatter_MeanDailyPrevalence_vs_", column, ".png"))
+plot_scatter("ClinicalDetections", "ClinicalDetections", paste0("scatter_ClinicalDetections_vs_", column, ".png"))
+plot_scatter("MeanDischargePrevalence", "MeanDischargePrevalence", paste0("scatter_MeanDischargePrevalence_vs_", column, ".png"))
+plot_scatter("ImportationPrevalence", "ImportationPrevalence", paste0("scatter_ImportationPrevalence_vs_", column, ".png"))
